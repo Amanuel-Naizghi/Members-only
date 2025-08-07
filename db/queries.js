34ade2880de321894d6aad115a11900af27dc351
Amanuel-Naizghi <naizghiaman@gmail.com>
2025-08-07 00:59:00 -0600

    User password has been encrypted for security by using bcryptjs

--- db/queries.js
+++ db/queries.js
@@ -1,9 +1,11 @@
 const pool = require('./pool');
+const bcrypt = require('bcryptjs')
 
 async function addUser(data){
+    const hashedPassword = await bcrypt.hash(data.password,10)
     await pool.query(`INSERT INTO users (firstName,lastName,password,status)
                       VALUES ($1 ,$2 ,$3 ,$4) RETURNING *`,
-                      [data.firstName,data.lastName,data.password,"guest"]
+                      [data.firstName,data.lastName,hashedPassword,"guest"]
                     );
 }
 
