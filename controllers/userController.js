65776296e8a62c8c02e475f330090c354772b808
Amanuel-Naizghi <naizghiaman@gmail.com>
2025-08-09 11:21:32 -0600

    Validation and sanitazion for the registration form has been done, any error will appear at the form for correction

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,46 @@
 const queries = require('../db/queries');
+const {body,validationResult} = require("express-validator");
 
-async function postAddUser(req,res){
-    const data = req.body;
-    await queries.addUser(data);
-    res.redirect('/');
-}
 
-module.exports = {postAddUser};
+const lengthErr1 = "length must be more than 8 characters";
+const lengthErr2 = "length must be more than 4 characters";
+
+const validateUser = [
+    body("password").trim()
+    .isLength({min:8}).withMessage(`Password ${lengthErr1}`),
+
+    body("confirmPassword")
+    .custom((value,{req}) =>{
+        if(value !== req.body.password){
+            throw new Error("Passwords do not match");
+        }
+        return true;
+    }),
+
+    body("userName")
+    .isLength({min:4,max:10}).withMessage(`User name ${lengthErr2}`),
+
+    body("lastName")
+    .custom((value,{req}) =>{
+        if(value === req.body.firstName){
+            throw new Error("First and last names could not be the same");
+        }
+        return true;
+    })
+];
+
+exports.postAddUser = [
+    validateUser,
+    async (req,res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.render('register',{
+                errors:errors.array(),
+                old:req.body
+            });
+        }
+        const data = req.body;
+        await queries.addUser(data);
+        res.redirect('/');
+    }
+]
